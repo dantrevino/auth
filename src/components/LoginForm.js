a6844230505491031b7c5604a844c7805634e5ce
Dan Trevino <dantrevino@gmail.com>
2017-02-21 23:16:27 -0800

    added login fields, initial cut at firebase auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email,password);
+  }
 
   render() {
     return(
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text=> this.setState({ text })}
-            style={{ height: 40, width: 100}} 
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection></CardSection>
         <CardSection>
-          <Button>
+          <Input
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            secureTextEntry
+          />
+        </CardSection>
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
